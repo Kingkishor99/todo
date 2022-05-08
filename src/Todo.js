c6a6f83f67ea8ca8dff0facc44fd106a8f140070
kishor patil <patilkk99@gmail.com>
2022-05-08 09:54:21 +0530

    third

--- src/Todo.js
+++ src/Todo.js
@@ -1,26 +1,48 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 function Todo() {
     const [a, setA] = useState([])
     const [text, setText] = useState("")
 
 
-    const pu = () => {
-        if (a.length < 12 && text) {
+    const pu = (e) => {
+        e.preventDefault()
+        if (a.length < 15 && text) {
             setA([...a, {
                 p: text
             }])
+
+
         }
         else console.log("kkkk")
 
 
         window.localStorage.setItem("a", JSON.stringify(a))
+
+
     }
+    const edit = (id) => {
+        // console.log(id)
+    }
+
+
+    const remove = (i) => {
+        console.log(i)
+        a.splice(i, 1)
+        setA(a)
+    }
+    const date = new Date()
+
 
 
     return (
         <div>
             <ol className='todo' >
+                <div>
+                    <span className='to-do'>To-Do list for {date.getDate()}-{date.getMonth()}-{date.getFullYear()} </span>
+
+                </div>
+
                 {a.map((c, i) =>
                     <div className='wrapp' key={i}>
 
@@ -28,14 +50,17 @@ function Todo() {
                         <li className='todolist'>{c.p}</li>
                         <div className='button'>
                             <input type='checkbox' className='checkbox' />
-                            <button className='edit'></button >
-                            <button className='delete'></button>
+                            <button onClick={() => edit(i)} className='edit'></button >
+                            <button onClick={() => remove(i)} className='delete'></button>
 
                         </div>
 
                     </div>)}
-                <input onChange={e => setText(e.target.value)} />
-                <button className='add' onClick={pu}><i className='calendar plus icon'>Add</i></button>
+
+                {(a.length < 15) ? (<div>
+                    <input id='input' onChange={e => setText(e.target.value)} placeholder="Enter task..." className="input" />
+                    <button className='add' onClick={e => pu(e)}><i className='calendar plus icon'></i>Add</button> </div>) : ("")}
+
 
             </ol>
         </div>
